0e14b2510ae1cc6e6969f9c48db80f085a7d2797
sergio rangel <sarangel@uniboyaca.edu.co>
2025-03-07 09:53:48 -0500

    Correcciones parte tablet

--- src/pages/inicio.js
+++ src/pages/inicio.js
@@ -1,48 +1,23 @@
-const modal =(texto) => {
-    const div = document.createElement('div');
-    div.classList.add('modal');
-    
-    const p = document.createElement('p');
-    p.textContent = texto;
+document.addEventListener("DOMContentLoaded", () => {
+    const btnPersonas = document.getElementById("btn-personas");
+    const btnEmpresas = document.getElementById("btn-empresas");
+    const tablaPersonas = document.getElementById("tabla-personas");
+    const tablaEmpresas = document.getElementById("tabla-empresas");
+    const titulo = document.getElementById("contact-title");
 
+    tablaPersonas.style.display = "table";
+    tablaEmpresas.style.display = "none";
+    titulo.textContent = "Lista de Contactos Personas";
 
-    const cerrarBtn = document.createElement('button');
-    cerrarBtn.textContent = 'Cerrar';
-    cerrarBtn.classList.add('btn');
-    cerrarBtn.addEventListener('click', () => {
-        div.remove();
+    btnPersonas.addEventListener("click", () => {
+        tablaPersonas.style.display = "table";
+        tablaEmpresas.style.display = "none";
+        titulo.textContent = "Lista de Contactos Personas";
     });
 
-    div.appendChild(p);
-    div.appendChild(cerrarBtn);
-
-    return div;
-}
-
-const inicioPage = () => {
-    const section = document.createElement('section');
-    
-    const boton = document.createElement('button');
-    boton.textContent = "Aceptar";
-    boton.classList.add('btn');
-    boton.addEventListener('click' , ()=>{
-        const widthScreen = window.innerWidth;
-        // if(widthScreen<=375){
-        //     alert('HOLA CPECITO');
-        // } else {
-        //     alert("HOLA PAPU");
-        // }
-        const msg = widthScreen <= 375 
-        ? modal("Hola Chocokrispies")
-        : modal("HOLA AMIGOS DEL YUTU");
-
-        section.appendChild(msg);
+    btnEmpresas.addEventListener("click", () => {
+        tablaPersonas.style.display = "none";
+        tablaEmpresas.style.display = "table";
+        titulo.textContent = "Lista de Contactos Empresas";
     });
-
-    section.appendChild(boton);
-
-    return section;
-};
-
-
-document.getElementById('page').appendChild(inicioPage());
+});
